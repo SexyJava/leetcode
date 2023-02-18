6669637b50846578c03adf862efaed6356921a02
liuyunda <man021436@163.com>
2023-02-18 21:09:04 +0800

    :tada: Questions56I,Questions56II

---
+++ src/com/leetcode/offer/Questions56II.java
@@ -0,0 +1,17 @@
+package com.leetcode.offer;
+
+/**
+ * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+ * @Author Liuyunda
+ * @Date 2023/2/18 21:07
+ */
+public class Questions56II {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+}
